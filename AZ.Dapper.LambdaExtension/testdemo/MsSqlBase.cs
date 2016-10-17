b58b9ef0f170532043147e6763a2f7649812896d
zhangjianqiao <rock_82@outlook.com>
2016-10-17 15:12:56 +0800

    12

---
+++ AZ.Dapper.LambdaExtension/testdemo/MsSqlBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace ConsoleApp1
+{
+    public class MsSqlBase
+    {
+        protected string Connstr = "data source=192.168.1.21;Database=test;Password=plus123456;User ID=root;";
+
+        public MsSqlBase()
+        {
+
+
+            // base(connstr);
+        }
+
+        public MsSqlBase(string connstr)
+        {
+
+            Connstr = connstr;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            var conn = new SqlConnection(Connstr);
+            conn.Open();
+            return conn;
+        }
+
+
+        public IDbConnection GetConnection(string strConn)
+        {
+            var conn = new SqlConnection(strConn);
+            conn.Open();
+            return conn;
+        }
+
+
+        public Tuple<bool, string> TestConn(string connstring)
+        {
+            bool isopen = false;
+            string msg = string.Empty;
+
+            try
+            {
+                var conn = GetConnection(connstring);
+                if (conn.State == ConnectionState.Open)
+                {
+                    isopen = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+
+            return new Tuple<bool, string>(isopen, msg);
+        }
+        public Tuple<bool, string> TestConn()
+        {
+            bool isopen = false;
+            string msg = string.Empty;
+
+            try
+            {
+                var conn = GetConnection(Connstr);
+                if (conn.State == ConnectionState.Open)
+                {
+                    isopen = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+
+            return new Tuple<bool, string>(isopen, msg);
+        }
+    }
+}
